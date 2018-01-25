c360c6c616fffad8bce41e20f515dc7acfaca9c5
Dragos Nicu <contact@dragosnicu.com>
2018-01-25 11:10:20 +0000

    something

--- config/urls.py
+++ config/urls.py
@@ -10,31 +10,31 @@ from photos.views import CarouselView
 urlpatterns = [
     # url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^$', CarouselView.as_view(), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    # url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
 
     # User management
-    url(r'^users/', include('wedding.users.urls', namespace='users')),
-    url(r'^accounts/', include('allauth.urls')),
+    # url(r'^users/', include('wedding.users.urls', namespace='users')),
+    # url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
-    ]
-    if 'debug_toolbar' in settings.INSTALLED_APPS:
-        import debug_toolbar
-        urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
-        ] + urlpatterns
+# if settings.DEBUG:
+#     # This allows the error pages to be debugged during development, just visit
+#     # these url in browser to see how these error pages look like.
+#     urlpatterns += [
+#         url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
+#         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
+#         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+#         url(r'^500/$', default_views.server_error),
+#     ]
+#     if 'debug_toolbar' in settings.INSTALLED_APPS:
+#         import debug_toolbar
+#         urlpatterns = [
+#             url(r'^__debug__/', include(debug_toolbar.urls)),
+#         ] + urlpatterns
